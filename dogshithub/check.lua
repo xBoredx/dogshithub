5011332dfb68dfeaf41db6a18bcbda8bea2f2ed1
Bored <72874749+xBoredx@users.noreply.github.com>
2022-05-28 12:52:28 +0300

    Update check.lua

--- dogshithub/check.lua
+++ dogshithub/check.lua
@@ -1,21 +1,21 @@
 if game.PlaceId == 6783611814 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/buildaboattosurvive.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/build%20a%20boat.lua"))()
 end
 if game.PlaceId == 6938803436 or game.PlaceId == 6990129309 or game.PlaceId == 7338881230 or game.PlaceId == 7274690025 or game.PlaceId == 6990131029 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/animedimension.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/Anime%20Dimension.lua"))()
 end
 if game.PlaceId == 6310438798 or game.PlaceId == 7055554318 or game.PlaceId == 9246859091 or game.PlaceId == 8546307710 or game.PlaceId == 7121379340 or game.PlaceId == 8546306408 or game.PlaceId == 8556363849 or game.PlaceId == 8264725073 or game.PlaceId == 8675240536 or game.PlaceId == 8251159599 or game.PlaceId == 8374021908 or game.PlaceId == 9125057272 or game.PlaceId == 9563012152 or game.PlaceId == 9696516573 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/animebrawler.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/Anime%20Brawler.lua"))()
 end
 if game.PlaceId == 5233782396 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/creatureofsonoria.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/sonaria.lua"))()
 end
 if game.PlaceId == 1224212277 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/madcity.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/Mad%20City.lua"))()
 end
 if game.PlaceId == 4282985734 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/combatwarrior.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/Combat%20Warrior.lua"))()
 end
 if game.PlaceId == 6969185078 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/broreallyplayingthisgame/Bird-Hub-Source/main/elementalawakening.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xBoredx/dogshithub/main/dogshithub/games/Elemental%20Awakening.lua"))()
 end
